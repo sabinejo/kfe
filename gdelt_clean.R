3bd55e13074263a9f5c99389bcb06ddb27882d60
Sunanda Garg <sunanda.garg>
2017-04-19 15:56:02 +0200

    basig daily aggregation and country resolution of gdelt

--- gdelt_clean.R
+++ gdelt_clean.R
@@ -6,32 +6,32 @@ library(readxl)
 countries = read_excel("Input/Country_codes_NAMO.xlsx", sheet = 1)
 # country_codes
 namo_country_codes = c(countries$Country_2)
-keep_namo <- function(cc_var) {
-  if(cc%in%namo_country_codes){
-    cc
-  } else {
-    NA
-  }
-}
 
 # gdelt data
-gdelt <- read.table("Data/gdelt_20170418.csv", header = T,sep=",")
+gdelt <- read.table("Data/gdelt_20170419.csv", header = T,sep=",")
+
+# aggregate daily events
+gdelt_events <- 
+  gdelt %>%
+  # only events in leading paragraphs, proxy of importance
+  filter(IsRootEvent==1) %>%  
+  # convert to date format, protest events, material conflict events
+  # EventRootCode = 14 (protest); Quadclass = 4; conflict
+  mutate(
+    event_date = as.Date(as.character(SQLDATE), "%Y%m%d"),
+    protest = ifelse(EventRootCode == 14,1,0),
+    material_conflict = ifelse(QuadClass == 4,1,0))%>% 
+  # removing misclassification of dates
+  filter(event_date > as.Date("20150101","%Y%m%d")) %>% 
+  # group by date, country
+  group_by(event_date,ActionGeo_CountryCode) %>%
+  # get daily aggregates of features and predictors
+  summarise(
+    total_events = n(),
+    protest_events = sum(protest),
+    conflict_events = sum(material_conflict)
+  )
 
 
-# country variable 
-country_code_vars <- 
-  gdelt %>% 
-  head() %>% 
-  select(Actor1Geo_ADM1Code,Actor2Geo_ADM1Code,ActionGeo_ADM1Code) %>% # keep the country vars
-  mutate_each(funs(cc = substr(.,1,2))) %>% # keep only first two characters
-  select(-Actor1Geo_ADM1Code,-Actor2Geo_ADM1Code,-ActionGeo_ADM1Code) %>% # remove extra vars
-  rowwise() %>% # rowwise comparison
-  mutate_each(funs(namo = keep_namo(.))) %>% # keep only namo countries
-  select(-Actor1Geo_ADM1Code_cc,-Actor2Geo_ADM1Code_cc,-ActionGeo_ADM1Code_cc) %>%  # remove extra vars
-  rowwise() %>% 
-  do(i = unique(.$Actor1Geo_ADM1Code_cc_namo,.$Actor2Geo_ADM1Code_cc_namo,.$ActionGeo_ADM1Code_cc_namo)).Last.value 
 
 
-
-gdelt %>% 
-  group_by()
